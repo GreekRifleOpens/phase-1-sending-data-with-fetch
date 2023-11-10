183efc9aff1e549f8c30d4aeaa655ba0e1cffc85
Hayder Abu AlHummos <greekrifleopens@gmail.com>
2023-11-10 13:00:24 +0300

    completed

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Add your code here
+function submitData(name, email) {
+  return fetch("http://localhost:3000/users", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json",
+    },
+    body: JSON.stringify({
+      name,
+      email,
+    }),
+  })
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      document.body.innerHTML = object["id"];
+    })
+    .catch(function (error) {
+      document.body.innerHTML = error.message;
+    });
+}
+
+// const formData = {
+//   dogName: "Byron",
+//   dogBreed: "Poodle",
+// };
+
+// const configurationObject = {
+//   method: "DELETE",
+//   headers: {
+//     "Content-Type": "application/json",
+//     Accept: "application/json",
+//   },
+//   body: JSON.stringify(formData),
+// };
+
+// fetch("http://localhost:3000/dogs", configurationObject)
+//   .then(function (res) {
+//     return res.json();
+//   })
+//   .then(function (obj) {
+//     console.log(obj);
+//   })
+//   .catch(function (error) {
+//     console.log(error.message);
+//   });
